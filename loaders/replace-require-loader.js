438ddfda0a7115dbd253ab64abf9e2ce48c386a2
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-11-22 23:57:35 +0800

    fix(replace-require-loader): update RegExp

--- loaders/replace-require-loader.js
+++ loaders/replace-require-loader.js
@@ -1,5 +1,5 @@
 module.exports = function replaceRequireLoader (source) {
-  return source.replace(/(const)\s+(.+)\s+?=\s+?__into_webpack5_require__\((.+)\)/img, ($0, $1, $2, $3) => {
+  return source.replace(/(const|let|var)\s+(.+)\s+?=\s+?__into_webpack5_require__\((.+)\)/img, ($0, $1, $2, $3) => {
     return `import ${$2} from ${$3}`
   })
 }
